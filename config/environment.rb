d76756a2cc62f64a7dc6280f9150f537dbf1ba61
nickgray06 <nickgray87@gmail.com>
2021-02-09 10:15:21 -0700

    commit tree 9447710ca2df9c4a733f0e89260c5a78766fa130

--- config/environment.rb
+++ config/environment.rb
@@ -1,1 +1,8 @@
 Dir[File.join(File.dirname(__FILE__), "../lib", "*.rb")].each {|f| require f}
+require 'pry'
+
+jim = Owner.new("Jim")
+
+
+#binding.pry
+0
--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,23 @@
 class Owner
   # code goes here
+  attr_reader :name, :species
+  @@all = []
+
+  def initialize name, species="human"
+    @name = name
+    @species = species
+
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def say_species
+    "I am a #{species}."
+    #binding.pry
+  end
+
+
 end
